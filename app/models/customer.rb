42000213d7eb4720afe65283374ceae2ae16c990
Dan Tong <danieltongnz@gmail.com>
2017-03-16 21:02:00 +1300

    Show sheet collections with date, and items collected on sheet collections index

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,6 @@ class Customer < ApplicationRecord
   enum kind: [ :ecolooper, :offsetter, :buyer ]
   validates_presence_of :name
   has_many :sheets
+  has_many :sheet_collection_items, through: :sheets
+  has_many :sheet_collections, through: :sheet_collection_items
 end
--- app/models/sheet_collection.rb
+++ app/models/sheet_collection.rb
@@ -1,5 +1,5 @@
 class SheetCollection < ApplicationRecord
-  has_many :sheet_collection_items
+  has_many :sheet_collection_items, dependent: :destroy
   has_many :sheets, through: :sheet_collection_items
   accepts_nested_attributes_for :sheet_collection_items
   validates_presence_of :date_collected
