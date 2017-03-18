626b17b3cd44d5b72afbe0483c444c98a062c41a
Dan Tong <danieltongnz@gmail.com>
2017-03-19 12:52:29 +1300

    Create sheets for customer from paper_types after creation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,11 @@ class Customer < ApplicationRecord
   has_many :sheets
   has_many :paper_collection_items, through: :sheets
   has_many :paper_collections, -> { distinct }, through: :paper_collection_items
+  after_create :create_sheets
+
+  def create_sheets
+    PaperType.find_each do |paper_type|
+      sheets.create(paper_type: paper_type)
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+PaperType.create!([
+  {name: 'blank_on_one_side', size: 'A4'},
+  {name: 'semi_blank', size: 'A4'},
+  {name: 'reject', size: 'A4'},
+  {name: 'blank_on_one_side', size: 'A3'},
+  {name: 'semi_blank', size: 'A3'},
+  {name: 'reject', size: 'A3'}
+])
