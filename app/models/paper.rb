7153598295308efc79ee6ecc7a3e99067d8c6532
Dan Tong <danieltongnz@gmail.com>
2017-03-28 20:59:44 +1300

    Sum and display quantities by ecoloop/offset/total

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -4,9 +4,16 @@ class Paper < ApplicationRecord
   has_many :paper_transactions
   # has_many :collections, through: :paper_transactions
 
-  scope :quantity_by_type, -> (name, size) { joins(:paper_type).find_by(paper_types: {name: name, size: size}).quantity_in_stock }
+  scope :quantity_by_type, -> (name, size, quantity) { joins(:paper_type).find_by(paper_types: {name: name, size: size}).quantity(quantity) }
 
-  def quantity_in_stock
-    paper_transactions.sum(:quantity)
+  def quantity(by_type = nil)
+    case by_type
+    when :ecoloop
+      paper_transactions.where(ecoloop_stock: true).sum(:quantity)
+    when :offset
+      paper_transactions.where(ecoloop_stock: false).sum(:quantity)
+    else
+      paper_transactions.sum(:quantity)
+    end
   end
 end
--- app/models/paper_type.rb
+++ app/models/paper_type.rb
@@ -1,5 +1,6 @@
 class PaperType < ApplicationRecord
   has_many :papers
+  has_many :paper_transactions, through: :papers
   validates_uniqueness_of :name, scope: :size
 
   def label
