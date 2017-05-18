bac1e3b3c1768867a411c63f2dfc7a575ed732c2
Dan Tong <danieltongnz@gmail.com>
2017-05-18 16:27:21 +1200

    Add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  BOOTSTRAP_FLASH_MSG = {
+    success: 'alert-success',
+    error: 'alert-danger',
+    alert: 'alert-warning',
+    notice: 'alert-info'
+  }
+
+  def bootstrap_class_for(flash_type)
+    BOOTSTRAP_FLASH_MSG.with_indifferent_access[flash_type]
+  end
 end
