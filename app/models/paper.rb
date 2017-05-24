681c47899bf2701f614ce1c1ae1fa7d4d1dab543
Dan Tong <danieltongnz@gmail.com>
2017-05-24 15:27:43 +1200

    Uncomment scopes for paper

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -4,10 +4,10 @@ class Paper < ApplicationRecord
   has_many :paper_transactions, dependent: :destroy
   # has_many :collections, through: :paper_transactions
 
-  # scope :for_offsetters, -> { joins(:customer).where(customers: {customer_type: :offsetter}) }
-  # scope :for_ecoloopers, -> { joins(:customer).where(customers: {customer_type: :ecolooper}) }
-  # scope :offset_by_type, -> (name, size) { joins(:paper_type).find_by(paper_types: {name: name, size: size}).offset_quantity }
-  # scope :ecoloop_by_type, -> (name, size) { joins(:paper_type).find_by(paper_types: {name: name, size: size}).ecoloop_quantity }
+  scope :for_offsetters, -> { joins(:customer).where(customers: {customer_type: :offsetter}) }
+  scope :for_ecoloopers, -> { joins(:customer).where(customers: {customer_type: :ecolooper}) }
+  scope :offset_by_type, -> (name, size) { joins(:paper_type).find_by(paper_types: {name: name, size: size}).offset_quantity }
+  scope :ecoloop_by_type, -> (name, size) { joins(:paper_type).find_by(paper_types: {name: name, size: size}).ecoloop_quantity }
   scope :excluding_rejects, -> { joins(:paper_type).where.not(paper_types: { name: 'reject' }) }
 
   def ecoloop_quantity
